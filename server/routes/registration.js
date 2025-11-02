79f6ad7bf06606582cbb667a5bb202f6f014bc4c
aichurek0990 <aichurek0990@gmail.com>
2025-11-02 15:40:15 -0800

    Added route to see all registered us ers for admin

--- server/routes/registration.js
+++ server/routes/registration.js
@@ -37,5 +37,17 @@ router.post("/", authMiddleware, verifyRole(["attendee"]), async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+//get all registered users
+
+router.get("/all", authMiddleware, verifyRole(["admin"]), async (req, res) => {
+  const result = await pool.query(
+    `SELECT r.id, u.name AS user, e.title AS event, r.ticket_type, r.created_at
+     FROM registrations r
+     JOIN users u ON r.user_id = u.id
+     JOIN events e ON r.event_id = e.id
+     ORDER BY r.created_at DESC`
+  );
+  res.json(result.rows);
+});
 
 export default router;
