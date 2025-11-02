09e97c1abc2d5dfbd983b3f8454195ae63a93f31
aichurek0990 <aichurek0990@gmail.com>
2025-11-02 10:34:08 -0800

    Initialize backend (Express, PostgreSQL, JWT, bcrypt, nodemon) and update root package.json

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// server/index.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./db.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// test
+app.get("/", (req, res) => {
+  res.send("🎉 Eventify API is running!Yay!");
+});
+
+// check db
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ message: "Database connected!", time: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// server
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT}`);
+});
