87d176ebe36807f064bbe776c28add99cb055c4e
aichurek0990 <aichurek0990@gmail.com>
2025-11-02 18:55:12 -0800

    Added Register page and connected it with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { useAuth } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import Register from "./components/Register";
 import "./App.css";
 
 function App() {
@@ -12,11 +13,8 @@ function App() {
       {user && <Navbar />}
 
       <Routes>
-        <Route
-          path="/"
-          element={user ? <Navigate to="/login" /> : <Login />}
-        />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
       </Routes>
     </Router>
   );
