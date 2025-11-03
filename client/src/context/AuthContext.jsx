6245442fc4ad9fc162ca321624fd3832a5ff82d5
aichurek0990 <aichurek0990@gmail.com>
2025-11-02 17:03:49 -0800

    Added AuthContext.jsx file

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // save user data
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  //check if we have user in localstorage
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  // login save token and user
+  const login = (userData, token) => {
+    setUser(userData);
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", token);
+  };
+
+  // logout 
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
